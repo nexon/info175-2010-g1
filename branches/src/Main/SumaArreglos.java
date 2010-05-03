c08abf3367dc18991dcbba06a88f15c9701eb6a0
es.orellana <es.orellana@826cc188-2a74-da15-414c-533c8f067582>
2010-05-03 18:40:57 +0000

    EO: hola hola coca cola xD

---
+++ branches/src/Main/SumaArreglos.java
@@ -0,0 +1,25 @@
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ package sumaarreglos;
+
+ /**
+ *
+ * @author albertolagos
+ */
+ public class SumaArreglos {
+ private double[] valores;
+
+
+ public double sumaValores() {
+ double valor = 0.0;
+ return valor;
+ }
+ public void sumaUnoAUno(SumaArreglos obj) {
+
+ }
+
+ }
