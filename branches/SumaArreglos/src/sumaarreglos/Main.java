7ae360bb4079742c53d529b9ec3bdbe28cb4f95b
alberto.lagos <alberto.lagos@826cc188-2a74-da15-414c-533c8f067582>
2010-05-03 19:22:11 +0000

    AL: funciones implementadas

--- branches/SumaArreglos/src/sumaarreglos/Main.java
+++ branches/SumaArreglos/src/sumaarreglos/Main.java
@@ -16,6 +16,21 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double[] aux = {1,2,3,4,5};
+        double[] aux2 = {2,3};
+
+        SumaArreglos test = new SumaArreglos(aux);
+        SumaArreglos test1 = new SumaArreglos(aux2);
+
+        System.out.println("ARREGLO 1: ");
+        test.imprimir();
+        System.out.println("ARREGLO 2: ");
+        test1.imprimir();
+
+        test.sumaUnoAUno(test1);
+        System.out.println("ARREGLO 1 MODIFICADO: ");
+        test.imprimir();
+
     }
 
 }
