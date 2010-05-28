6123eb829d6239d50bcd7350d5b7b845aa98c5fe
alberto.lagos <alberto.lagos@826cc188-2a74-da15-414c-533c8f067582>
2010-05-28 19:31:45 +0000

    AL: 	- Agregadas clases principales.

---
+++ src/pronosticator/Ciudad.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pronosticator;
+
+/**
+ *
+ * @author albertolagos
+ */
+public class Ciudad {
+    private String nombre;
+    private Tiempo pronostico;
+}
